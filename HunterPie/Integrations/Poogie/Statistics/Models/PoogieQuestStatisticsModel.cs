5d38f4b0c53b915683ee6ca49d2cd949b40552a8
WetU <yuk6196@gmail.com>
2023-02-01 17:36:38 +0900

    Revert commits

---
+++ HunterPie/Integrations/Poogie/Statistics/Models/PoogieQuestStatisticsModel.cs
@@ -0,0 +1,37 @@
+﻿using HunterPie.Features.Statistics.Models;
+using HunterPie.Integrations.Poogie.Common.Models;
+using Newtonsoft.Json;
+using System;
+using System.Linq;
+
+namespace HunterPie.Integrations.Poogie.Statistics.Models;
+
+internal record PoogieQuestStatisticsModel(
+    [property: JsonProperty("game_type")] GameType GameType,
+    [property: JsonProperty("started_at")] DateTime StartedAt,
+    [property: JsonProperty("finished_at")] DateTime FinishedAt,
+    [property: JsonProperty("player")] PoogiePlayerStatisticsModel[] Players,
+    [property: JsonProperty("monsters")] PoogieMonsterStatisticsModel[] Monsters,
+    [property: JsonProperty("hash")] string Hash
+)
+{
+    public HuntStatisticsModel ToEntity() =>
+        new HuntStatisticsModel(
+            Game: GameType.ToEntity(),
+            StartedAt: StartedAt,
+            FinishedAt: FinishedAt,
+            Players: Players.Select(it => it.ToEntity()).ToList(),
+            Monsters: Monsters.Select(it => it.ToEntity()).ToList(),
+            Hash: Hash
+        );
+
+    public static PoogieQuestStatisticsModel From(HuntStatisticsModel model) =>
+        new PoogieQuestStatisticsModel(
+            GameType: model.Game.ToApiModel(),
+            StartedAt: model.StartedAt,
+            FinishedAt: model.FinishedAt,
+            Players: model.Players.Select(PoogiePlayerStatisticsModel.From).ToArray(),
+            Monsters: model.Monsters.Select(PoogieMonsterStatisticsModel.From).ToArray(),
+            Hash: model.Hash
+        );
+}
