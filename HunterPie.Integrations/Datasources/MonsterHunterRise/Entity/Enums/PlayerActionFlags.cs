b0476921ff9a8ab88e1055f1b872b20ba3339a4b
WetU <yuk6196@gmail.com>
2023-02-02 18:55:49 +0900

    merged release branch

--- HunterPie.Integrations/Datasources/MonsterHunterRise/Entity/Enums/PlayerActionFlags.cs
+++ HunterPie.Integrations/Datasources/MonsterHunterRise/Entity/Enums/PlayerActionFlags.cs
@@ -3,12 +3,6 @@
 [Flags]
 public enum ActionFlags : uint
 {
-<<<<<<<< HEAD:HunterPie.Integrations/Datasources/MonsterHunterRise/Entity/Enums/PlayerActionFlags.cs
     None = 0,
     MaximumMight = 1 << 5
 }
-========
-    Blue,
-    Red
-}
->>>>>>>> origin/release/v2.7.0:HunterPie.Integrations/Datasources/MonsterHunterRise/Entity/Enums/Scroll.cs
