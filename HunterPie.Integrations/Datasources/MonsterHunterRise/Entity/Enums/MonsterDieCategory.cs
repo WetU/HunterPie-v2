8f35211139d6603634c8f3be8d523cce810e1aec
WetU <yuk6196@gmail.com>
2023-06-23 18:06:56 +0900

    Disposing dead or captured monsters

---
+++ HunterPie.Integrations/Datasources/MonsterHunterRise/Entity/Enums/MonsterDieCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace HunterPie.Integrations.Datasources.MonsterHunterRise.Entity.Enums;
+
+public enum MonsterDieCategory : ushort
+{
+    Normal = 0,
+    HyakuryuEscape = 1,
+    Capture = 2,
+    Max = 3,
+    Invalid = 65535
+}
